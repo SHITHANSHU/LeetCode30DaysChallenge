68fe7a4849e81b168a79ab779038d956b735d599
SHITHANSHU <shithanshu24121998@gmail.com>
2020-05-16 23:01:57 +0530

    day 16

---
+++ LeetCode30DaysChallengeMay2020/Week3/OddEvenLinkedList.cpp
@@ -0,0 +1,66 @@
+//Question
+
+/*
+
+Odd Even Linked List
+Solution
+Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL
+Output: 1->3->5->2->4->NULL
+Example 2:
+
+Input: 2->1->3->5->6->4->7->NULL
+Output: 2->3->6->7->1->5->4->NULL
+Note:
+
+The relative order inside both the even and odd groups should remain as it was in the input.
+The first node is considered odd, the second node even and so on ...
+
+*/
+
+//Solution
+
+/*
+
+Time Complexity O(n)
+Space Complexity O(1)
+
+*/
+
+//Solution Code
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL)
+            return NULL;
+        ListNode* start=head;
+        ListNode* secondStart=head->next;
+        ListNode* secondStartTop=head->next;
+        
+        while(secondStart!=NULL && secondStart->next!=NULL)
+        {
+            start->next=secondStart->next;
+            start=start->next;
+            secondStart->next=start->next;
+            secondStart=secondStart->next;
+        }
+        start->next=secondStartTop;
+        return head;
+    }
+};
