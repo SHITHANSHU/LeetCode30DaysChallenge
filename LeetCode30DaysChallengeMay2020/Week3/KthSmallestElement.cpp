279a8e3ca595799cb81830ba85c04ee6a3612906
SHITHANSHU <shithanshu24121998@gmail.com>
2020-05-20 19:29:40 +0530

    Day20

---
+++ LeetCode30DaysChallengeMay2020/Week3/KthSmallestElement.cpp
@@ -0,0 +1,84 @@
+//Question 
+/*
+Kth Smallest Element in a BST
+Solution
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Example 1:
+
+Input: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+Output: 1
+Example 2:
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+Output: 3
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+
+   Hide Hint #1  
+Try to utilize the property of a BST.
+   Hide Hint #2  
+Try in-order traversal. (Credits to @chan13)
+   Hide Hint #3  
+What if you could modify the BST node's structure?
+   Hide Hint #4  
+The optimal runtime complexity is O(height of BST).
+
+*/
+
+//Solution 
+/*
+
+Time complexity O(n)
+Space Complexity O(n)
+
+*/
+
+//solution
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+vector<int> my_vec;
+    void getInorder(TreeNode* node) 
+    { 
+        if(!node)
+            return;
+        getInorder(node->left); 
+        my_vec.push_back(node->val);
+        getInorder(node->right);
+    } 
+    
+    int kthSmallest(TreeNode* root, int k) {
+        if(!root)
+            return -1;
+        getInorder(root);
+        return my_vec[k-1];
+            
+    }
+};
